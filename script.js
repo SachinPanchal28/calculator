22e85eb1e6554c966580356ad2ca20065ecaea05
SachinPanchal28 <sachinp@gmail.com>
2022-02-01 10:43:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function calc(n) {
+    if (n == "="){
+        document.forms.t1.value = eval('document.forms.t1.value')
+    } else {
+        document.forms.t1.value = document.forms.t1.value+n
+    }
+
+ 
+}
