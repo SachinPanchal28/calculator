f95829749ab077757ec5ca2a327c19521421fd0c
SachinPanchal28 <sachinp1628@gmail.com>
2022-05-15 10:18:56 +0530

    commited

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function calc(n) {
     if (n == "="){
-        document.forms.t1.value = eval('document.forms.t1.value')
+        document.forms.t1.value = eval('document.forms.t1.value');
     } else {
-        document.forms.t1.value = document.forms.t1.value+n
+        document.forms.t1.value = document.forms.t1.value+n;
     }
-
- 
 }
